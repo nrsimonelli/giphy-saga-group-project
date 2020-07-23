c893faab7323027bd5f9a827c47967aa845ab022
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-07-23 10:20:07 -0500

    task list and db setup

--- database.sql
+++ database.sql
@@ -1,8 +1,5 @@
 CREATE DATABASE "giphy_search_favorites";
 
--- You'll need a table for storing each giphy image favorite
--- Each favorite image can be assigned 1 of the following categories as a Foreign Key
-
 -- Category table
 CREATE TABLE "category" (
     "id" SERIAL PRIMARY KEY,
@@ -12,3 +9,23 @@ CREATE TABLE "category" (
 -- Default categories. You may change them :)
 INSERT INTO "category" ("name")
 VALUES ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
+
+-- You'll need a table for storing each giphy image favorite
+-- Each favorite image can be assigned 1 of the following categories as a Foreign Key
+CREATE TABLE "favorite" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (100) NOT NULL,
+    "url" VARCHAR (100) NOT NULL,
+    "description" VARCHAR (500),
+    "category_id" INT REFERENCES "category"
+
+);
+
+INSERT INTO "favorite" ("name", "url", "category_id")
+VALUES ('cat', ';adjsg[', 1),('hat', 'napoifas', 2), ('mat', ';ha;seihf', 3);
+
+SELECT * FROM category;
+SELECT * FROM favorite;
+
+SELECT * FROM category
+JOIN favorite ON favorite.category_id = category.id;
---
+++ taskList.md
@@ -0,0 +1,21 @@
+### To Dos ###
+> Eric, Nick, Mase
+
+## SQL ##
+- [ ] Whiteboard table relationships
+- [ ] Spin up SQL syntax
+- [ ] Create necessary tables and/or structure
+
+## Search View ##
+- [ ] Search interface, input field for string
+    - [ ] API syntax / Search Endpoint
+    - [ ] dropdown for number of display
+- [ ] Place to display result
+- [ ] Button to favorite
+- [ ] ID information to save + category to store
+
+## Favorites View
+- [ ] GET to DB for favorites 
+- [ ] Map and display on page (actual images)
+- [ ] Allow user to select category for image from db
+- [ ] 
